5be2bf5f214de61876f11b3a8a9c3183e7b49d88
Ram Valliyappan <Ram_Valliyappan@hotmail.com>
2013-07-14 06:26:05 +0000

    web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World - my startup is getting along!');
+  	
+	response.send(fs.readFileSync('index.html'));
 });
 
 var port = process.env.PORT || 5000;
